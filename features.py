a1901d1f07addb2a96180f2e64fe30cefc9f5d8e
Swapnil <swapnil@auberginesolutions.com>
2017-06-29 19:44:24 +0530

    +readme, +start_letter, end_letter to features

--- features.py
+++ features.py
@@ -1,6 +1,8 @@
 def features(name):
     features = {}
     features['alwayson'] = True
+    features['starts_with'] = name.lower()[0]
+    features['end_with'] = name.lower()[-1]
     for letter in 'abcdefghijklmnopqrstuvwxyz':
         features['count(%s)' % letter] = name.lower().count(letter)
         features['has(%s)' % letter] = letter in name.lower()
--- use_classifier.py
+++ use_classifier.py
@@ -1,9 +1,13 @@
 import pickle
 from features import features
 
-f = open('classifier.pickle', 'rb')
-classifier = pickle.load(f)
-f.close()
+try:
+    f = open('classifier.pickle', 'rb')
+    classifier = pickle.load(f)
+    f.close()
+    classifier.show_most_informative_features(n=10)
+    print "Classifier result: " + classifier.classify(features("Swapnil"))
 
-classifier.show_most_informative_features(n=10)
-print "Classifier result: " + classifier.classify(features("Swapnil"))
+except:
+    print "Prepare the classifier using - train_classifier.py and then try to use."
+
